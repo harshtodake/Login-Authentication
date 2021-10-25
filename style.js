0a4b258077ab847a9ded130baea33206b191d25d
harshtodake <djharshraj@gmail.com>
2021-10-25 20:50:46 +0530

    25-10-2021-validation-added

--- style.js
+++ style.js
@@ -15,3 +15,30 @@ const loginText = document.querySelector(".title-text .login");
            signupBtn.click();
            return false;
          });
+
+
+
+
+  // var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate() {
+var username = document.getElementById("email").value;
+var password = document.getElementById("psw").value;
+if ( username == "harsh@gmail.com" || password == "Harsh@123"){
+alert ("Login successfully");
+window.location.assign("https://www.tutorialrepublic.com"); // Redirecting to other page.
+return false;
+}
+else{
+// attempt --;// Decrementing by one.
+// alert("You have left "+attempt+" attempt;");
+alert("Please try again !");
+// Disabling fields after 3 attempts.
+// if( attempt == 0){
+// document.getElementById("email").disabled = true;
+// document.getElementById("psw").disabled = true;
+// document.getElementById("login").disabled = true;
+// return false;
+// }
+}
+}
